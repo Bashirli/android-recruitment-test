b3dc053b1200480a18cd13610364dc28027f7bc4
Bashirli <bashirli1903@gmail.com>
2024-03-29 15:05:26 +0400

    network connectivity check , web socket connectivity check finished

--- app/src/main/java/com/bashirli/algoritmatask/common/network/NetworkConnectivityObserver.kt
+++ app/src/main/java/com/bashirli/algoritmatask/common/network/NetworkConnectivityObserver.kt
@@ -1,25 +1,42 @@
-package com.isteam.movieappaz.common.network
+package com.bashirli.algoritmatask.common.network
 
-import android.content.Context
 import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
-import dagger.hilt.android.qualifiers.ApplicationContext
-import javax.inject.Inject
+import android.net.Network
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
 
-class NetworkConnectivityObserver @Inject constructor(
-    @ApplicationContext val context: Context
-) {
-    fun isNetworkAvailable(): Boolean {
-        val connectivityManager =
-            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val nw = connectivityManager.activeNetwork ?: return false
-        val actNw = connectivityManager.getNetworkCapabilities(nw) ?: return false
-        return when {
-            actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-            actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-            actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
-            actNw.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) -> true
-            else -> false
+class NetworkConnectivityObserver(
+    private val connectivityManager: ConnectivityManager
+) : ConnectivityObserver {
+
+    override fun connectionObserve(): Flow<ConnectivityObserver.ConnectionStatus> = callbackFlow {
+        val callback = object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                super.onAvailable(network)
+                launch { send(ConnectivityObserver.ConnectionStatus.Available) }
+            }
+
+            override fun onLosing(network: Network, maxMsToLive: Int) {
+                super.onLosing(network, maxMsToLive)
+                launch { send(ConnectivityObserver.ConnectionStatus.Losing) }
+            }
+
+            override fun onLost(network: Network) {
+                super.onLost(network)
+                launch { send(ConnectivityObserver.ConnectionStatus.Lost) }
+            }
+
+            override fun onUnavailable() {
+                super.onUnavailable()
+                launch { send(ConnectivityObserver.ConnectionStatus.Unavailable) }
+            }
         }
-    }
+
+        connectivityManager.registerDefaultNetworkCallback(callback)
+        awaitClose { connectivityManager.unregisterNetworkCallback(callback) }
+    }.distinctUntilChanged()
+
 }
