9763be3ec0451a9d6e2418e060c912dc8ede8ff5
Bashirli <bashirli1903@gmail.com>
2024-03-29 15:54:34 +0400

    adapter finished

--- app/src/main/java/com/bashirli/algoritmatask/presentation/ui/home/InvestAdapter.kt
+++ app/src/main/java/com/bashirli/algoritmatask/presentation/ui/home/InvestAdapter.kt
@@ -1,11 +1,17 @@
 package com.bashirli.algoritmatask.presentation.ui.home
 
+import android.graphics.Color
+import android.graphics.DashPathEffect
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bashirli.algoritmatask.common.base.BaseAdapter
 import com.bashirli.algoritmatask.databinding.ItemInvestBinding
 import com.bashirli.algoritmatask.domain.model.InvestResultUiModel
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 
 class InvestAdapter : BaseAdapter<InvestResultUiModel>() {
 
@@ -14,11 +20,67 @@ class InvestAdapter : BaseAdapter<InvestResultUiModel>() {
         fun find(item: InvestResultUiModel) {
             with(binding) {
                 data = item
-
+                setChart(
+                    listOf(
+                        Entry(10f, item.x2.toFloat()),
+                        Entry(40f, item.x3.toFloat()),
+                        Entry(60f, item.x4.toFloat()),
+                    )
+                )
                 executePendingBindings()
             }
         }
+
+        private fun setChart(values: List<Entry>) {
+            with(binding) {
+                chart.setBackgroundColor(Color.TRANSPARENT)
+                chart.description.isEnabled = false
+
+                chart.setTouchEnabled(true)
+                chart.setDragEnabled(true)
+                chart.setScaleEnabled(true)
+                chart.setPinchZoom(true)
+
+                val set1: LineDataSet
+
+                if (chart.data != null && chart.data.dataSetCount > 0) {
+                    set1 = chart.data.getDataSetByIndex(0) as LineDataSet
+                    set1.values = values
+                    set1.notifyDataSetChanged()
+                    chart.data.notifyDataChanged()
+                    chart.notifyDataSetChanged()
+                } else {
+                    set1 = LineDataSet(values, "Price")
+
+                    set1.setDrawIcons(false)
+                    set1.enableDashedLine(10f, 0f, 0f)
+                    set1.color = Color.BLACK
+                    set1.setCircleColor(Color.BLACK)
+                    set1.lineWidth = 1f
+                    set1.circleRadius = 3f
+
+
+                    // draw points as solid circles
+                    set1.setDrawCircleHole(false)
+
+                    // customize legend entry
+                    set1.formLineWidth = 1f
+                    set1.formLineDashEffect = DashPathEffect(floatArrayOf(10f, 5f), 0f)
+                    set1.formSize = 5f
+                    set1.valueTextSize = 15f
+
+
+                    set1.setFillColor(Color.BLACK)
+                    val dataSets = ArrayList<ILineDataSet>()
+                    dataSets.add(set1)
+                    val data = LineData(dataSets)
+                    chart.data = data
+                }
+            }
+        }
     }
+
+
 
     override fun onCreate(
         parent: ViewGroup,
